564053c81d743d7f1beae83ad98a8413f131a5be
Pau Santana <127206784+psantana5@users.noreply.github.com>
2023-06-15 15:52:38 +0200

    Add files via upload
    Created menus and functions for the base firewall use. Starting point.

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,241 @@
 #include <iostream>
+#include <limits>
 
+// Function prototypes
+void displayMainMenu();
+int getValidChoice(int min, int max);
+void handleMainMenuChoice(int choice);
+void displayFilteringMenu();
+void handleFilteringMenuChoice(int choice);
+void displayRuleManagementMenu();
+void handleRuleManagementMenuChoice(int choice);
+void displaySecurityMenu();
+void handleSecurityMenuChoice(int choice);
 
-void menu() {
-    std::cout << "Firewall main menu" << std::endl;
-    std::cout << "1. View Firewall Rules" << std::endl;
-    std::cout << "2. Add Firewall Rule" << std::endl;
-    std::cout << "3. Remove Firewall Rule" << std::endl;
-    std::cout << "4. Save Firewall Configuration" << std::endl;
-    std::cout << "5. Exit" << std::endl;
-    std::cout << "" << std::endl;
+// Function to display the main menu
+void displayMainMenu()
+{
+    std::cout << "========== Firewall Menu ==========\n";
+    std::cout << "1. Packet Filtering\n";
+    std::cout << "2. Rule Management\n";
+    std::cout << "3. Security Management\n";
+    std::cout << "4. Exit\n";
+    std::cout << "===================================\n";
     std::cout << "Enter your choice: ";
+}
 
+// Function to validate and retrieve a choice within a given range
+int getValidChoice(int min, int max)
+{
     int choice;
-    std::cin >> choice;
+    while (true)
+    {
+        std::cout << "Enter your choice: ";
+        std::cin >> choice;
 
-    switch (choice) {
+        if (std::cin.fail() || choice < min || choice > max)
+        {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "Invalid choice. Please try again.\n";
+        }
+        else
+        {
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            break;
+        }
+    }
+
+    return choice;
+}
+
+// Function to handle user input and navigate main menu options
+void handleMainMenuChoice(int choice)
+{
+    switch (choice)
+    {
         case 1:
-            std::cout << "View Firewall Rules" << std::endl;
+            displayFilteringMenu();
             break;
         case 2:
-            std::cout << "Add Firewall Rule" << std::endl;
+            displayRuleManagementMenu();
             break;
         case 3:
-            std::cout << "Remove Firewall Rule" << std::endl;
+            displaySecurityMenu();
             break;
         case 4:
-            std::cout << "Save Firewall Config" << std::endl;
-            break;
-        case 5:
-            std::cout << "Exiting..." << std::endl;
-            break;
+            std::cout << "Exiting the Firewall Program. Goodbye!\n";
+            // Implement any cleanup or necessary steps before exiting
+            exit(0);
         default:
-            std::cout << "Invalid choice!" << std::endl;
-            break;
+            std::cout << "Invalid choice. Please try again.\n";
     }
 }
 
-int main() {
-    menu();
+// Function to display the filtering menu
+void displayFilteringMenu()
+{
+    std::cout << "========== Filtering Menu ==========\n";
+    std::cout << "1. Packet Filtering\n";
+    std::cout << "2. Deep Packet Inspection\n";
+    std::cout << "3. Exit to Main Menu\n";
+    std::cout << "====================================\n";
+}
+
+// Function to handle user input and navigate filtering menu options
+void handleFilteringMenuChoice(int choice)
+{
+    switch (choice)
+    {
+        case 1:
+            std::cout << "Packet Filtering feature selected.\n";
+            // Implement the logic for packet filtering
+            break;
+        case 2:
+            std::cout << "Deep Packet Inspection feature selected.\n";
+            // Implement the logic for deep packet inspection
+            break;
+        case 3:
+            std::cout << "Returning to the main menu.\n";
+            break;
+        default:
+            std::cout << "Invalid choice. Please try again.\n";
+    }
+}
+
+// Function to display the rule management menu
+void displayRuleManagementMenu()
+{
+    std::cout << "========== Rule Management Menu ==========\n";
+    std::cout << "1. Create Rule\n";
+    std::cout << "2. Delete Rule\n";
+    std::cout << "3. Exit to Main Menu\n";
+    std::cout << "=========================================\n";
+}
+
+// Function to handle user input and navigate rule management menu options
+void handleRuleManagementMenuChoice(int choice)
+{
+    switch (choice)
+    {
+        case 1:
+            std::cout << "Create Rule feature selected.\n";
+            // Implement the logic for rule creation
+            break;
+        case 2:
+            std::cout << "Delete Rule feature selected.\n";
+            // Implement the logic for rule deletion
+            break;
+        case 3:
+            std::cout << "Returning to the main menu.\n";
+            break;
+        default:
+            std::cout << "Invalid choice. Please try again.\n";
+    }
+}
+
+// Function to display the security menu
+void displaySecurityMenu()
+{
+    std::cout << "========== Security Management Menu ==========\n";
+    std::cout << "1. User Authentication Menu\n";
+    std::cout << "2. Logs Analyzer\n";
+    std::cout << "3. Availability Settings\n";
+    std::cout << "4. VPN Management\n";
+    std::cout << "5. Threat Prevention\n";
+    std::cout << "6. Exit to Main Menu\n";
+    std::cout << "============================================\n";
+}
+
+// Function to handle user input and navigate security menu options
+void handleSecurityMenuChoice(int choice)
+{
+    switch (choice)
+    {
+        case 1:
+            std::cout << "User Authentication Menu feature selected.\n";
+            // Implement the logic for user authentication menu
+            break;
+        case 2:
+            std::cout << "Logs Analyzer feature selected.\n";
+            // Implement the logic for logs analyzer
+            break;
+        case 3:
+            std::cout << "Availability Settings feature selected.\n";
+            // Implement the logic for availability settings
+            break;
+        case 4:
+            std::cout << "VPN Management feature selected.\n";
+            // Implement the logic for VPN management
+            break;
+        case 5:
+            std::cout << "Threat Prevention feature selected.\n";
+            // Implement the logic for threat prevention
+            break;
+        case 6:
+            std::cout << "Returning to the main menu.\n";
+            break;
+        default:
+            std::cout << "Invalid choice. Please try again.\n";
+    }
+}
+
+int main()
+{
+    int choice;
+
+    while (true)
+    {
+        displayMainMenu();
+        choice = getValidChoice(1, 4);
+
+        handleMainMenuChoice(choice);
+
+        if (choice == 1)
+        {
+            while (true)
+            {
+                displayFilteringMenu();
+                choice = getValidChoice(1, 3);
+
+                handleFilteringMenuChoice(choice);
+
+                if (choice == 3)
+                    break; // Return to the main menu
+            }
+        }
+        else if (choice == 2)
+        {
+            while (true)
+            {
+                displayRuleManagementMenu();
+                choice = getValidChoice(1, 3);
+
+                handleRuleManagementMenuChoice(choice);
+
+                if (choice == 3)
+                    break; // Return to the main menu
+            }
+        }
+        else if (choice == 3)
+        {
+            while (true)
+            {
+                displaySecurityMenu();
+                choice = getValidChoice(1, 6);
+
+                handleSecurityMenuChoice(choice);
+
+                if (choice == 6)
+                    break; // Return to the main menu
+            }
+        }
+
+        std::cout << "Press Enter to continue...";
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cin.get(); // Wait for Enter key before clearing the screen or displaying the menu again
+        system("clear"); // Use "cls" instead of "clear" on Windows
+    }
+
     return 0;
 }
